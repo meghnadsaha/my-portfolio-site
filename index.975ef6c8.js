a0d38ed9166ac0b3ef5d5fc1d119b11eecac1c43
meghnadsaha <meghnadsaha422@gmail.com>
2024-07-03 13:44:24 +0530

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27087,8 +27087,8 @@ var _stylesCss = require("./styles.css");
  *
  * If you don't have one of the social sites listed, leave it as an empty string.
  */ const siteProps = {
-    name: "Meghnad  Saha",
-    title: "Web Designer & Content Creator",
+    name: "Meghnad  Saha ",
+    title: "Full Stack Dev & Content Creator",
     email: "alex@example.com",
     gitHub: "microsoft",
     instagram: "microsoft",
